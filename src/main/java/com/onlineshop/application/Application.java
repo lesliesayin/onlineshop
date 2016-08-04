1062ff744f90cf28985d11783a921e192db5e328
lesliesayin <sayin.leslieanne@gmail.com>
2016-08-04 16:53:07 +0800

    Initial commit

---
+++ src/main/java/com/onlineshop/application/Application.java
@@ -0,0 +1,36 @@
+package com.onlineshop.application;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+/**
+ * Created by training on 8/4/16.
+ */
+@SpringBootApplication
+@ComponentScan("com.onlineshop")
+public class Application {
+    @Autowired
+    private Environment environment;
+
+    public static void main(String args[]) {
+        SpringApplication.run(Application.class, args);
+    }
+
+//    @Bean
+//    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+//        return new JdbcTemplate(dataSource);
+//    }
+//
+//    @Bean
+//    public DataSource dataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName(environment.getProperty("spring.datasource.driverClassName"));
+//        dataSource.setUrl(environment.getProperty("spring.datasource.url"));
+//        dataSource.setUsername(environment.getProperty("spring.datasource.username"));
+//        dataSource.setPassword(environment.getProperty("spring.datasource.password"));
+//        return dataSource;
+//    }
+}
---
+++ src/main/java/com/onlineshop/controller/CustomerController.java
@@ -0,0 +1,15 @@
+package com.onlineshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by training on 8/4/16.
+ */
+@Controller
+@RequestMapping("/customers")
+public class CustomerController {
+
+
+
+}
